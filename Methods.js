6df490ac26f8fdf16d2a0b2179f83e11a6440b53
aupontj1 <aupontj@outlook.com>
2022-03-09 18:03:31 -0500

    Create Methods.js

---
+++ Methods.js
@@ -0,0 +1,21 @@
+const add = function (y,z) {
+    return y + z
+}
+
+const math = {
+    numbers:[1,2,3,4,5],
+    add: function (y,z){
+        return y + z;
+    },
+    multiply: function (y,z){
+        return y + z;
+    },
+    divide: function (y,z){
+        return y / z;
+    },
+    cubed: function (x){
+        return x * x * x;
+    }
+
+}
+
